98c7800587ec1dc0ac2bbbc32b0858aed10fa4b6
emtseng <etseng42@gmail.com>
2017-05-05 11:14:09 -0400

    adds bootstrap row/col styling to editor

--- app/components/Editor.js
+++ app/components/Editor.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { fetchNouns, setNouns } from '../reducers/analyze';
-import {addStory} from '../reducers/setText';
-import {browserHistory} from 'react-router';
+import { addStory } from '../reducers/setText';
+import { browserHistory } from 'react-router';
 import Actors from './Actors';
 import axios from 'axios';
 import querystring from 'querystring';
@@ -18,39 +18,67 @@ class Editor extends Component {
     this.onSceneTextChange = this.onSceneTextChange.bind(this);
     this.onGenerateActors = this.onGenerateActors.bind(this);
   }
-
-  // TO DO: figure out how to create multiple actors and do `setScenes` for each...
   onSubmit(event) {
     event.preventDefault();
-    axios.post('/api/stories', {title: event.target.storyTitle.value})
-    .then(newStory => {
-      const storyId = newStory.data.id;
-      return axios.post(`/api/stories/${storyId}/scenes`, {paragraphs: this.state.textBody})
-    })
-    .then(newScene => {
-      // unsure why a newScene isn't returned to this .then, but the log does say a post request to /api/stories/:storyId/scenes was successful...
-      const sceneId = newScene.data.id;
-      axios.post(`/api/actors/${sceneId}/bulk`, {actors: this.props.nouns})
-    })
+    axios.post('/api/stories', { title: event.target.storyTitle.value })
+      .then(newStory => {
+        const storyId = newStory.data.id;
+        return axios.post(`/api/stories/${storyId}/scenes`, { paragraphs: this.state.textBody })
+      })
+      .then(newScene => {
+        const sceneId = newScene.data.id;
+        axios.post(`/api/actors/${sceneId}/bulk`, { actors: this.props.nouns })
+      })
   }
-
-  onSceneTextChange (event) {
-    this.setState({textBody: event.target.value});
+  onSceneTextChange(event) {
+    this.setState({ textBody: event.target.value });
   }
-  onGenerateActors (event) {
+  onGenerateActors(event) {
     event.preventDefault();
     this.props.parseNouns(this.state.textBody);
   }
   render() {
     return (
-      <div className="storyInput">
+      <div className="storyEditor">
         <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <input type="text" placeholder="Enter Story Title Here" name="storyTitle"/>
-            <textarea rows="100" cols="78" type="text" className="form-control" placeholder="Enter story here" name="fullStory" onChange={this.onSceneTextChange} />
+          <div className="row">
+            <div className="form-group col-md-6">
+              <input
+                type="text"
+                placeholder="Enter Story Title Here"
+                name="storyTitle"
+              />
+              <textarea
+                rows="10"
+                cols="78"
+                type="text"
+                className="form-control"
+                placeholder="Enter story here"
+                name="fullStory"
+                onChange={this.onSceneTextChange}
+              />
+            </div>
+            <div className="col-md-6">
+              <div className="buttonContainer">
+                <button
+                  className="btn btn-default"
+                  onClick={this.onGenerateActors}
+                >
+                  Generate Actors
+              </button>
+              </div>
+            </div>
           </div>
-          <button className="btn btn-default" onClick={this.onGenerateActors} >Generate Actors</button>
-          <button type="submit">Submit Scene</button>
+          <div className="row">
+            <div className="buttonContainer">
+              <button
+                className="btn btn-default"
+                type="submit"
+              >
+                Publish
+            </button>
+            </div>
+          </div>
         </form>
         <Actors />
       </div>
@@ -69,13 +97,13 @@ const mapStateToProps = (store, ownProps) => {
 };
 
 function mapDispatchToProps(dispatch) {
-    return {
-      parseNouns: (input) => {
-        dispatch(fetchNouns(input));
-      },
-      setStory: (input) => {
-        dispatch(addStory(input));
-      }
+  return {
+    parseNouns: (input) => {
+      dispatch(fetchNouns(input));
+    },
+    setStory: (input) => {
+      dispatch(addStory(input));
+    }
   };
 }
 
