638da7531a845608f50a94cf15b353cd63dd993b
Jacob Kayen <jacob.kayen@gmail.com>
2017-05-08 18:41:54 -0400

    refactor: parseProNouns first attempt to pull from wiki for autofill

--- server/utils/findProperNouns.js
+++ server/utils/findProperNouns.js
@@ -1,22 +1,26 @@
-export default function findProperNouns(text) {
-  return sortWords(pronounParser(text));
-}
+import wiki from 'wikijs';
 
-// this function will return an array of pronouns from a string of text
-const pronounParser = str => {
-  let arr = str.split('\n').join(' '); // elliminate new paragraphs
+// remove new lines and spaces
+const splitStr = str => {
+  let arr = str.split('\n').join(' ');
   arr = arr.split(' ');
-  arr = arr.filter(word => {
+  return arr;
+};
+
+const removeAbbr = arr => {
+  return arr.filter(word => {
     return word // convert string to array of words; only include words (no empty strings)
-      && word !== 'Mr.'
-      && word !== 'Ms.'
-      && word !== 'Mrs.'
-      && (word.length > 2 || word[word.length - 1] !== '.'); // removes middle initials as to not confuse them with the end of a sentence
+        && word !== 'Mr.'
+        && word !== 'Ms.'
+        && word !== 'Mrs.'
+        && (word.length > 2 || word[word.length - 1] !== '.'); // removes middle initials as to not confuse them with the end of a sentence
   });
-  arr = arr.filter((word, i, a) => {
+};
+
+const filterWords = arr => {
+  return arr.filter((word, i, a) => {
     let prevWord = a[i - 1];
-    return word
-      && i !== 0 // exclude the first word (will always be capitalized)
+    return i !== 0 // exclude the first word (will always be capitalized)
       && /^[a-zA-Z]/.test(arr[i]) // exclude anything that does not start with a letter
       && !word.includes('—') // excludes any two words that have been joined by an em-dash
       && !word.includes('-') // excludes and two words that have been joined by a hyphen
@@ -24,14 +28,20 @@ const pronounParser = str => {
       && prevWord[prevWord.length - 1] !== '!' // excludes first word of a sentence
       && prevWord[prevWord.length - 1] !== '?' // excludes first word of a sentence
       && prevWord[prevWord.length - 2] !== '.' // excludes first word of a sentence if previous sentence ended in close quote
-  })
-  .map(word => word.replace(/'s/g, '')) // remove 's from end of words
-  .map(word =>  word.replace(/\W+/g, "")); // remove any non letter characters (i.e. extraneous quotes)
-  // remove any date words
-  arr = arr.filter(word => {
-    const dateWords = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Mon', 'Tue', 'Wed', 'Thurs', 'Fri',  'Sat', 'Sun'];
-    return !dateWords.includes(word);
-  })
+  });
+};
+
+const removePunctuation = arr => {
+  return arr.map(word => word.replace(/'s/g, '')) // remove 's from end of words
+            .map(word =>  word.replace(/\W+/g, '')); // remove any non letter characters (i.e. extraneous quotes)
+};
+// remove any date words
+const removeWords = arr => {
+  const dateWords = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Mon', 'Tue', 'Wed', 'Thurs', 'Fri',  'Sat', 'Sun'];
+  return arr.filter(word => !dateWords.includes(word));
+};
+
+const arrToObj = arr => {
   let pronounObj = {};
   let i = 0;
   while (i < arr.length) {
@@ -46,30 +56,34 @@ const pronounParser = str => {
     i++;
   }
   return pronounObj;
-}
+};
 
 // convert obj of words to array by rate of occurrence
-const sortWords = objOfWords => {
+const sortObjByOccurrence = obj => {
   const hash = {};
-  let arrayOfOccurrences = [];
-  let arrayOfWords = [];
-  for (const word in objOfWords) {
-    if (hash[objOfWords[word]]) {
-      hash[objOfWords[word]].push({
+  for (const word in obj) {
+    if (hash[obj[word]]) {
+      hash[obj[word]].push({
         title: word,
-        description: null,
-        image: null,
-        link: null
+        description: '',
+        image: '',
+        link: ''
       });
     } else {
-      hash[objOfWords[word]] = [{
+      hash[obj[word]] = [{
         title: word,
-        description: null,
-        image: null,
-        link: null
+        description: '',
+        image: '',
+        link: ''
       }];
     }
   }
+  return hash;
+};
+
+const convertHashToOrderedArr = hash => {
+  let arrayOfOccurrences = [];
+  let arrayOfWords = [];
   for (const num in hash) {
     arrayOfOccurrences.push(+num);
   }
@@ -78,6 +92,19 @@ const sortWords = objOfWords => {
     arrayOfWords = arrayOfWords.concat(hash[key]);
   })
   return arrayOfWords;
-}
+};
 
+const getWikiData = arr => {
+  return arr.map(actorObj => {
+    actorObj.description = wiki().page(actorObj.title)
+    .then(page => page.summary())
+    .then(data => data.slice(0, 250));
+    return actorObj;
+  });
+};
+
+export default function findProperNouns(text) {
+  let arr = convertHashToOrderedArr(sortObjByOccurrence(arrToObj(removeWords(removePunctuation(filterWords(removeAbbr(splitStr(text))))))));
+  return getWikiData(arr);
+}
 
--- webpack.config.js
+++ webpack.config.js
@@ -39,5 +39,8 @@ module.exports = {
   },
   plugins: devMode
     ? [new LiveReloadPlugin({appendScriptTag: true})]
-    : []
-}
+    : [],
+  externals: {
+    'isomorphic-fetch': 'fetch'
+  }
+};
