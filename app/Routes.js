ed4f8a5bcd8dc4f709f22e712df7d96ef15b60c0
emtseng <etseng42@gmail.com>
2017-05-10 17:43:43 -0400

    fix: renames ViewStory in Routes

--- app/Routes.js
+++ app/Routes.js
@@ -8,7 +8,7 @@ import Signup from './components/Signup';
 import MapTest from './components/Map';
 
 import Editor from './components/Editor';
-import Story from './components/Story';
+import ViewStory from './components/ViewStory';
 
 /* -----------------    COMPONENT     ------------------ */
 
@@ -18,8 +18,8 @@ const Routes = (props) => (
       <IndexRoute component={Home} />
       <Route path="login" component={Login} />
       <Route path="signup" component={Signup} />
-      <Route path="stories/fakeStory" component={Story} onEnter={props.onFakeStoryEnter} />
-      <Route path="stories/:storyId" component={Story} onEnter={props.onRealStoryEnter} />
+      <Route path="stories/fakeStory" component={ViewStory} onEnter={props.onFakeStoryEnter} />
+      <Route path="stories/:storyId" component={ViewStory} onEnter={props.onRealStoryEnter} />
       <Route path="editor" component={Editor} />
       <Route path="maptest" component={MapTest} />
       <Route path="*" component={Home} />
