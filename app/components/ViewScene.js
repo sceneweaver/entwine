3531b7f90c30a3641d0f47f8624b574b03b203f1
Jacob Kayen <jacob.kayen@gmail.com>
2017-05-10 19:24:53 -0400

    update2

--- app/components/ViewScene.js
+++ app/components/ViewScene.js
@@ -13,16 +13,6 @@ class Scene extends Component {
     return (
       <div className="container-fluid">
         <div className="row">
-<<<<<<< HEAD
-          <div className="col-md-1">
-          </div>
-          <div className="col-md-8 col-md-offset-1 article-text article-font">
-            <div dangerouslySetInnerHTML={this.setInnerHTML(this.props.html)} />
-          </div>
-          <div className="col-md-2 pull-right">
-            <ViewActors />
-=======
-
           <div className="col-md-4">
             <div className="article-titles article-font col-md-offset-1">
               <h3>{this.props.storyTitle}</h3>
@@ -33,19 +23,8 @@ class Scene extends Component {
               dangerouslySetInnerHTML={this.setInnerHTML(this.props.html)}
             />
           </div>
-
           <div className="col-md-8 pull-right actorsBlock">
-            {this.props.actors ? <h3 className="actors-heading article-font">Actors</h3> : null}
-            {this.props.actors ?
-              this.props.actors.map(actor => (
-                <div key={actor.name}>
-                  <img className="img-responsive img-circle actors-display" src={actor.image} />
-                  <h5 className="article-font">{actor.name}</h5>
-                  <p>{actor.description}</p>
-                </div>
-              ))
-              : null}
->>>>>>> 631c3bcceef290be93b2f6a8ad5c180b3b19e46a
+            <ViewActors />
           </div>
 
         </div>
