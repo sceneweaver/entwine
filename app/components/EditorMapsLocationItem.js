60bd10237dcd60c44931054dd1b204b4f4a79d9c
emtseng <etseng42@gmail.com>
2017-05-11 15:00:09 -0400

    feat: readies editormaps for demo

---
+++ app/components/EditorMapsLocationItem.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+/* ----- COMPONENT ----- */
+
+class EditorMapsLocationItem extends Component {
+  render() {
+    const location = this.props.location
+        , index = this.props.index;
+    return (
+      <div className="actor-item">
+
+        <div className="actor-btns">
+          <button
+            className="btn btn-default"
+            onClick={this.props.onDeleteLocation.bind(this, this.props.index)}
+          >
+            <span className="glyphicon glyphicon-trash" ></span>
+          </button>
+        </div>
+
+        <div className="actor-info">
+          <div className="actor-name-field-container">
+            <label>Location:</label>
+            <input
+              type="text"
+              className="actor-name-field"
+              value={location.name}
+              onChange={this.props.onLocationsChange.bind(this, index, 'name')}
+            />
+          </div>
+        </div>
+
+    </div>);
+  }
+}
+
+/* ----- CONTAINER ----- */
+
+import { connect } from 'react-redux';
+import { changeLocation, deleteLocation } from '../reducers/editor';
+
+const mapStateToProps = (state, ownProps) => ({
+  position: ownProps.position,
+  index: ownProps.index,
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onLocationsChange(locationIndex, field, event) {
+    event.preventDefault();
+    event.stopPropagation();
+    dispatch(changeLocation(ownProps.position, locationIndex, field, event.target.value));
+  },
+  onDeleteLocation(locationIndex, event) {
+    event.preventDefault();
+    event.stopPropagation();
+    dispatch(deleteLocation(ownProps.position, locationIndex));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditorMapsLocationItem);
