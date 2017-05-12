b098e33c7baacb08e9290e36f3cf46e5058f5576
emtseng <etseng42@gmail.com>
2017-05-12 11:04:11 -0400

    fix: imports material-ui slider as a const from the component so natasha can build the project

--- app/components/Views/ViewStorySlider.js
+++ app/components/Views/ViewStorySlider.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import Slider from 'material-ui/slider';
+import {Slider} from 'material-ui';
 
 /* ----- COMPONENT STYLES ----- */
 
