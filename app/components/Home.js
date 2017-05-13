eb63aa213a635482de853a3c34862721031b9880
Natasha Desai <tashadesai@gmail.com>
2017-05-13 16:36:01 -0400

    styling hom page cards

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,76 +1,78 @@
 import React from 'react';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText, RaisedButton} from 'material-ui';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 
 
 const Home = () => (
-  <div >
-  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-  <div className="home main-featured container">
-    <div className="row feature-row">
-
-      <div className="featured-card col-md-8">
-      <Card>
-        <CardMedia
-          overlay={<CardTitle title="Featured Story" subtitle="James Comey's Conspicuous Independence" />}
-        >
-        <img height="400px" src="https://westernqueensland.files.wordpress.com/2014/05/new-york-sattelite-1.jpg" />
-        </CardMedia>
-
-      </Card>
+  <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+    <div className="home main-featured container">
+      <div className="row feature-row">
+        <div className="featured-card col-md-8">
+          <Card>
+            <CardMedia
+              overlay={<CardTitle title="Featured Story" subtitle="James Comey's Conspicuous Independence" />}
+            >
+              <img height="400px" src="https://westernqueensland.files.wordpress.com/2014/05/new-york-sattelite-1.jpg" />
+            </CardMedia>
+          </Card>
+        </div>
+        <div className="col-md-4 about">
+          <Card>
+            <CardMedia>
+            </CardMedia>
+            <CardText style={{'font-size': 25}}>entwine allows content creators to quickly embed interactive maps, information about key characters, and data visualizations into their stories.</CardText>
+            <div className="create-story-button">
+              <RaisedButton label="Create a Story Now" fullWidth={true} labelStyle={{'font-size': 22}}/>
+            </div>
+          </Card>
+        </div>
       </div>
-      <div className="col-md-4 about">
-       <Card>
-        <CardMedia>
-        </CardMedia>
-        <CardText>Entwine allows content creators to quickly embed interactive maps, information about key characters, and data visualizations into their stories.</CardText>
-      </Card>
+
+      <div className="row small-featured-stories feature-row">
+        <div className="col-md-3">
+          <Card>
+            <CardMedia
+              overlay={<CardTitle title="Featured Story" />}
+            >
+            <img src="https://westernqueensland.files.wordpress.com/2014/05/new-york-sattelite-1.jpg" />
+            </CardMedia>
+          </Card>
+          </div>
+          <div className="col-md-3">
+          <Card>
+            <CardMedia
+              overlay={<CardTitle title="Featured Story" />}
+            >
+              <img src="https://westernqueensland.files.wordpress.com/2014/05/new-york-sattelite-1.jpg" />
+            </CardMedia>
+          </Card>
+        </div>
+        <div className="col-md-3">
+          <Card>
+            <CardMedia
+              overlay={<CardTitle title="Featured Story" />}
+            >
+              <img src="https://westernqueensland.files.wordpress.com/2014/05/new-york-sattelite-1.jpg" />
+            </CardMedia>
+          </Card>
+        </div>
+        <div className="col-md-3">
+          <Card>
+            <CardMedia
+              overlay={<CardTitle title="Featured Story" />}
+            >
+              <img src="https://westernqueensland.files.wordpress.com/2014/05/new-york-sattelite-1.jpg" />
+            </CardMedia>
+          </Card>
+        </div>
       </div>
     </div>
-
-    <div className="row small-featured-stories feature-row">
-      <div className="col-md-3">
-      <Card>
-        <CardMedia
-          overlay={<CardTitle title="Featured Story" />}
-        >
-        <img src="https://westernqueensland.files.wordpress.com/2014/05/new-york-sattelite-1.jpg" />
-        </CardMedia>
-      </Card>
-      </div>
-      <div className="col-md-3">
-      <Card>
-        <CardMedia
-          overlay={<CardTitle title="Featured Story" />}
-        >
-        <img src="https://westernqueensland.files.wordpress.com/2014/05/new-york-sattelite-1.jpg" />
-        </CardMedia>
-      </Card>
-      </div>
-      <div className="col-md-3">
-      <Card>
-        <CardMedia
-          overlay={<CardTitle title="Featured Story" />}
-        >
-        <img src="https://westernqueensland.files.wordpress.com/2014/05/new-york-sattelite-1.jpg" />
-        </CardMedia>
-      </Card>
-      </div>
-      <div className="col-md-3">
-      <Card>
-        <CardMedia
-          overlay={<CardTitle title="Featured Story" />}
-        >
-        <img src="https://westernqueensland.files.wordpress.com/2014/05/new-york-sattelite-1.jpg" />
-        </CardMedia>
-      </Card>
-      </div>
-    </div>
-  </div>
   </MuiThemeProvider>
-  </div>
 );
 
 export default Home;
